0c9599191211a00e7aa9095903b9bf01db53126f
jadenswang <jaden.shiteng.wang@gmail.com>
2018-09-02 12:11:28 -0400

    expand to multipage layout

--- docs/loadelement.js
+++ docs/loadelement.js
@@ -1,17 +1,3 @@
-function load(div) {
-
-    var x = [
-        "RP", "PVP", "Servers", "Mods"];
-
-    for (var i = 0; i < x.length; i++) {
-        document.getElementById(x[i]).style.display = "none";
-    }
-
-    console.log(document.getElementById(div).style.display = "block");
-}
-
-window.onload = load('Servers');
-
 function copy(toCopy) {
     document.getElementById('ip').value = toCopy
     var copyText = document.getElementById("ip");
